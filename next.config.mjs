0af872e44f8f43ccb2509872ac9cd517018861f5
Danilo Gomes <danilo.gomes@cilia.com.br>
2025-10-07 08:12:03 -0300

    add ajustes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist', 
   basePath: '/danilocgomes.dev', 
   eslint: {
     ignoreDuringBuilds: true,
